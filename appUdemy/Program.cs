b39a5a7108047604625b46d0db98723a3eff7e66
YilberEspitia <yilber.espitia@grantitan.com>
2022-03-18 12:44:48 -0500

    Se agrego paquete newtonsoft

--- appUdemy/Program.cs
+++ appUdemy/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace appUdemy
 {
+    public class Producto
+    {
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public string Descripcion { get; set; }
+        public bool Estado { get; set; }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Producto producto = new Producto
+            {
+                Nombre = "Camisa Volcom",
+                Precio = 800,
+                Descripcion = "Tela de algodón",
+                Estado = true
+            };
+            string json = JsonConvert.SerializeObject(producto, Formatting.Indented);
+            Console.WriteLine(json);
         }
     }
 }
